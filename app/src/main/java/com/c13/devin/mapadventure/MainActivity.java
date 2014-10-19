c452f03b5f1ac023e4d15e3c180fad2a21d7396c
devin <devincorrigall@gmail.com>
2014-10-18 23:36:59 -0700

    Added my location to the map.

--- app/src/main/java/com/c13/devin/mapadventure/MainActivity.java
+++ app/src/main/java/com/c13/devin/mapadventure/MainActivity.java
@@ -3,11 +3,22 @@ package com.c13.devin.mapadventure;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        GoogleMap googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+
+        googleMap.setMyLocationEnabled(true);
+
     }
 }
